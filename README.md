c15f2f67defb57acfbd131f68a53d43a6baacce6
hobos-taco <hobos_taco@hotmail.com>
2016-05-01 18:09:39 +1000

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+ForgeAdmin Permissions
+==============
+
+[![Build Status](https://travis-ci.org/forgeAdmin/fadmin-permissions.svg?branch=master)](https://travis-ci.org/forgeAdmin/fadmin-permissions)
---
+++ src/main/java/com/github/fadmin/fadminpermissions/common/CommonProxy.java
@@ -0,0 +1,18 @@
+package com.github.fadmin.fadminpermissions.common;
+
+import net.minecraftforge.fml.common.event.*;
+
+public class CommonProxy {
+
+  public void preload(FMLPreInitializationEvent event) {}
+
+  public void load(FMLInitializationEvent event) {}
+
+  public void postload(FMLPostInitializationEvent event) {}
+
+  public void serverAboutToStart(FMLServerAboutToStartEvent event) {}
+
+  public void serverStarted(FMLServerStartedEvent event) {}
+
+  public void serverStopped(FMLServerStoppedEvent event) {}
+}
